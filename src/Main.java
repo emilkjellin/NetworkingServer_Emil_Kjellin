57cbb2e7403fb1a3c84b4e39e9577159282a41dc
emilkjellin <emil.kjellin@elev.ga.ntig.se>
2025-05-12 08:54:11 +0200

    la in servern

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,69 @@
+import java.net.*;
+import java.io.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ServerSocket server = null;
+        Socket client;
+
+        // Default port number we are going to use
+        int portnumber = 5050;
+        if (args.length >= 1){
+            portnumber = Integer.parseInt(args[0]);
+        }
+
+        // Create Server side socket
+        try {
+            server = new ServerSocket(portnumber);
+        } catch (IOException ioe) {
+            System.out.println("Cannot open socket. " + ioe);
+            System.exit(1);
+        }
+
+        System.out.println("ServerSocket is created " + server);
+
+        // Wait for data from client and reply
+        while (true) {
+            try {
+                // Listens for a connection to be made
+                // to this socket and accepts it.
+                // The method blocks until a connection is made
+                System.out.println("Waiting for connect request...");
+                client = server.accept();
+
+                System.out.println("Connect request is accepted...");
+
+                String clientHost = client.getInetAddress().getHostAddress();
+                int clientPort = client.getPort();
+                System.out.println("Client host = " + clientHost +
+                        " Client port = " + clientPort);
+
+                // Read data from the client
+                InputStream clientIn = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
+                String msgFromClient = br.readLine();
+                System.out.println("Message received from client = " + msgFromClient);
+
+                // Send response to the client
+                if (msgFromClient != null && !msgFromClient.equalsIgnoreCase("bye")) {
+                    OutputStream clientOut = client.getOutputStream();
+                    PrintWriter pw = new PrintWriter(clientOut, true);
+                    String ansMsg = "Hello, " + msgFromClient;
+                    pw.println(ansMsg);
+                }
+
+                // Close sockets
+                if (msgFromClient != null && msgFromClient.equalsIgnoreCase("bye")) {
+                    server.close();
+                }
+                client.close();
+
+            } catch (IOException ioe) {
+                System.out.println("Exception caught when trying to listen on port "
+                        + portnumber + " or listening for a connection");
+                System.out.println(ioe.getMessage());
+            }
+        }
     }
 }
